8706c2feb673e3a426ebee0c49ab4d44f320506b
Garvin <gming27@gmail.com>
2019-08-03 22:02:41 -0400

    Moves stackclass file to W5D5 dir

--- W5D5/stack.rb
+++ W5D5/stack.rb
@@ -2,15 +2,11 @@ class Stack
 
     def initialize
         @stack = []
-        
-
-
     end
 
     def push(el)
         #adds an element to the stack
         stack << el
-
     end
 
     def pop
@@ -20,10 +16,10 @@ class Stack
 
     def peek
         #returns, but doesn't remove, the top element in the stack
-        stack[last]
+        stack.last
     end
 
+    private
 
-    
-
+    attr_accessor :stack
 end
