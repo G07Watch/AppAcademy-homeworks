8ad37fc9714f314a42fecd7fbb1e742baf0799ed
Garvin <gming27@gmail.com>
2019-08-19 11:34:18 -0400

    Adds Playwrights ORM

--- W7D5/skeleton/plays.rb
+++ W7D5/skeleton/plays.rb
@@ -81,25 +81,76 @@ class Play
 end
 
 class Playwright 
+  attr_accessor :id, :name, :birth_year
 
   def self.all
+    PlayDBConnection.instance.execute(<<-SQL,
+      SELECT
+        *
+      FROM
+        playwrights;  
+      SQL
   end
 
   def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, name
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        playwrights.name = ?;
+      SQL
   end
 
-  def initialize()
+
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
   def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights(name, birth_year)
+      VALUES
+        (?,?);
+    SQL
   end
 
   def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+    UPDATE
+      playwrights
+    SET
+      name = ?, birth_year = ?
+    WHERE
+      playwrights.id = ?;
+      SQL
   end
 
-  def get_plays
+  def get_plays(name)
+    PlayDBConnection.instance.execute(<<-SQL, self.name)
+    SELECT 
+      *
+    FROM
+      plays
+    WHERE
+      plays.playwright_id = (
+        SELECT
+          playwrights.id
+        FROM
+          playwrights
+        WHERE
+          playwrights.name = ?
+      );
+    SQL
   end
-  
+
 
 
   
