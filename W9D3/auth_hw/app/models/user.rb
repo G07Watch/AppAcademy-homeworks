3e60cb86ebc02c8481de56de590dada6630644e0
Garvin <gming27@gmail.com>
2019-08-30 00:27:53 -0400

    Adds Helpers, Layouts, and Partials

--- W9D3/auth_hw/app/models/user.rb
+++ W9D3/auth_hw/app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
     attr_reader :password
-    before_validation :ensure_session_token
-    validates :username, :session_token,  presence: true
+    after_initialization :ensure_session_token
+    validates :username, :session_token,  presence: true, uniqueness: true
     validates :password_digest, presence: { message: 'Password can\'t be blank'} 
     validates :password, length: { minimum: 6, allow_nil: true}
 
 
 
     def password=(password)
+        @password = password
         self.password_digest = BCrypt::Password.create(password)
-
     end
 
     def is_password?(password)
@@ -19,10 +19,10 @@ class User < ApplicationRecord
     def self.find_by_credentials(username,password)
 
         user = User.find(username)
-        if user.is_password?(password)
+        if user && user.is_password?(password)
             return user
         else
-            raise 'Incorrect password'
+            return nil 
         end
         
     end
@@ -32,12 +32,11 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        user = self
-        user.session_token = User.generate_session_token
+        self.update!(session_token: self.class.generate_session_token)
     end
 
     def ensure_session_token
-        self.session_token || = User.generate_session_token
+        self.session_token || = self.class.generate_session_token
     end
 
 
