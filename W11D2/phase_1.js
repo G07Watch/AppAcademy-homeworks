a0c1640a6a6374447e6e9a9a0e9f425654b6b753
Garvin <gming27@gmail.com>
2019-09-11 10:38:53 -0400

    Updates W11D2 Hw

--- W11D2/phase_1.js
+++ W11D2/phase_1.js
@@ -4,20 +4,22 @@ function titleize (array, cb)
     let bigger = []
     
     bigger = array.map( (el) => {
-     ["Mx.", el, "Jingleheimer", "Schmidt"]
+      return ["Mx.", el, "Jingleheimer", "Schmidt"]
     });
     
     cb(bigger);
 };
 
-function print (arr) {
-    console.log(arr);
-}
+// function print (arr) {
+//     console.log(arr);
+// }
 
-// const print = function (arr){ 
-//     arr.forEach( function (el){
-//      console.log(el)}
-//      );
-// };
+const print = function (arr){ 
+    arr.forEach( function (el){
+     console.log(el)}
+     );
+};
 
-titleize(["Mary", "Brian", "Leo"], print());
+titleize(["Mary", "Brian", "Leo"], print);
+
+print([['boop']]);
