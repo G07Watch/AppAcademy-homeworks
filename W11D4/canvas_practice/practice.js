025ea16ec8b62cdcbae19ac0f022d01eec1dc28e
Garvin <gming27@gmail.com>
2019-09-13 13:08:37 -0400

    Updates Canvas Exercises so it actually renders

--- W11D4/canvas_practice/practice.js
+++ W11D4/canvas_practice/practice.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    
+    const canvas = document.getElementById('mycanvas');
+    canvas.width = 500;
+    canvas.width = 500;
+    const ctx = canvas.getContext('2d')
+
+    ctx.fillStyle = "red";
+    ctx.fillRect(10, 10, 500, 500);
+
+
+    ctx.beginPath();
+    ctx.arc(200, 100, 75, 0, 2 * Math.PI);
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 10;
+    ctx.stroke();
 
 
 
 
 });
 
-const canvas = document.getElementById('mycanvas');
-const ctx = canvas.getContext('2d')
-
-canvas.fillStyle = "red";
-canvas.fillRect(10, 10, 500, 500);
-
-
-ctx.beginPath();
-ctx.arc(200, 100, 75, 0, 2 * Math.PI);
-ctx.strokeStyle = 'blue';
-ctx.lineWidth = 10;
-ctx.stroke();
