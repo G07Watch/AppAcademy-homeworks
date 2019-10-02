de4f73026b725e8336103ce25d982bb06ba6e84f
Garvin <gming27@gmail.com>
2019-10-02 10:43:23 -0400

    Adds Rails Auth to W14D2

---
+++ W14D2/RailsAuth/RailsAuth/app/controllers/application_controller.rb
@@ -0,0 +1,22 @@
+class ApplicationController < ActionController::Base
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
